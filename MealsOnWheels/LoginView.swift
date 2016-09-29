e08307f2b10f1b72b6c34137b315e41593bb06ae
Rahul Ajmera <rahulajmera@lawn-128-61-11-70.lawn.gatech.edu>
2016-09-29 15:07:48 -0400

    Worked on LoginView

--- MealsOnWheels/LoginView.swift
+++ MealsOnWheels/LoginView.swift
@@ -31,31 +31,46 @@ class LoginView: UIView {
     }
     
     func configureTextFields() {
+        
         emailTF.placeholder = "Email"
+        emailTF.layer.cornerRadius = 10.0
+        emailTF.layer.borderWidth = 1
+        emailTF.textColor = UIColor.white
+        
+        
         
         passwordTF.placeholder = "Password"
+        passwordTF.layer.cornerRadius = 10.0
+        passwordTF.layer.borderWidth = 1
+        passwordTF.textColor = UIColor.white
+
     }
     
     func configureButtons() {
         loginBtn.setTitle("Login", for: UIControlState())
         loginBtn.setTitleColor(UIColor.blue, for: UIControlState())
         loginBtn.backgroundColor = UIColor.white
+        loginBtn.layer.cornerRadius = 20.0
+        loginBtn.layer.borderColor = UIColor.white.cgColor
+        loginBtn.layer.borderWidth = 1
+
         
         signUpBtn.setTitle("Sign Up", for: UIControlState())
         signUpBtn.setTitleColor(UIColor.blue, for: UIControlState())
         signUpBtn.backgroundColor = UIColor.white
+        
     }
     
     func configureView() {
         
-//        let gradient = CAGradientLayer()
-//        gradient.frame = self.view.bounds
-//        gradient.colors = [MWConstants.colors.loginDarkGradient.cgColor, MWConstants.colors.loginLightGradient.cgColor]
-//        self.view.layer.insertSublayer(gradient, at: 0)
+        let gradient = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = [MWConstants.colors.loginDarkGradient.cgColor, MWConstants.colors.loginLightGradient.cgColor]
+        self.layer.insertSublayer(gradient, at: 0)
 
-        self.backgroundColor = MWConstants.colors.loginBackground
+//        self.backgroundColor = MWConstants.colors.loginBackground
         
-        configureImageViews()
+        //configureImageViews()
         configureTextFields()
         configureButtons()
         
@@ -70,11 +85,13 @@ class LoginView: UIView {
         
         self.prepareViewsForAutoLayout(viewsDict as! [String : UIView])
         
-        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:|-20-[logo]-20-[login]-20-[signup]", views: viewsDict as [String : AnyObject]))
+        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:|-50-[emTF]-30-[psTF]-100-[login]-30-[signup]", views: viewsDict as [String : AnyObject]))
         
-        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[logo]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
+        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[emTF]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
         
-        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[login(==100)]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
+        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[psTF]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
+        
+        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[login]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
         
         self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[signup]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
         
